d8c03db80754cd31c2b7d89fcc30769fdf531cc0
joão <joao.santana@grupokrs.com.br>
2020-11-29 16:04:39 -0300

    feat: configurando o deploy do Firebase e adicionando o Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <React.Fragment>
       <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <title>JP Santana</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
