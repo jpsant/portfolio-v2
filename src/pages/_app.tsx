660e0488b65b22bea507d419d5d723e773ba39dd
joão <joao.santana@grupokrs.com.br>
2020-12-06 22:48:36 -0300

    feat: adicionando a lib NProgress, para mostrar uma barra de loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,16 @@ import type { AppProps } from 'next/app';
 import '../i18n';
 import '../foundations/global.scss';
 import '../foundations/fonts.scss';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Head from 'next/head';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -13,6 +22,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         <title>JP Santana</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
