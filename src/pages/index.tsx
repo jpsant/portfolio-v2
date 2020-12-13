6195726878295647ac3170e63ebef795cc19fc0c
joão <joao.santana@grupokrs.com.br>
2020-12-13 15:06:00 -0300

    refac: atualizando a page principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
-import SoonTemplate from '~templates/soonTemplate';
+import HomeTemplate from '~templates/HomeTemplate';
+import AboutTemplate from '~templates/AboutTemplate';
+import SkillsTemplate from '~templates/SkillsTemplate';
+import ProjectsTemplate from '~templates/ProjectsTemplate';
+import FutureTemplate from '~templates/FutureTemplate';
+import ContactTemplate from '~templates/ContactTemplate';
+import Footer from '~organisms/Footer';
 
 const App = () => {
-  return <SoonTemplate />;
+  return (
+    <>
+      <HomeTemplate />
+      <AboutTemplate />
+      <SkillsTemplate />
+      <ProjectsTemplate />
+      <FutureTemplate />
+      <ContactTemplate />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
