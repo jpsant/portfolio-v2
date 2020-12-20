1e7bfab74e83489164188c3aa6e8bbbf437b890e
joão <joao.santana@grupokrs.com.br>
2020-12-20 15:52:31 -0300

    refac: refatorando o component do SectionTitle

--- src/components/atoms/SectionTitle/index.tsx
+++ src/components/atoms/SectionTitle/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 const SectionTitle = ({ title, img, theme }: Props) => {
   return (
     <div className={`sectionTitle ${theme}`}>
-      <h1 className="sectionTitle-h1">{title}</h1>
+      <h1 className={`sectionTitle-h1`}>{title}</h1>
       <img className="sectionTitle-img" src={img} alt={title + ' image'} />
     </div>
   );
