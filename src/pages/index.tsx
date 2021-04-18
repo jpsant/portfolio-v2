6d85d3ab1d0fde5ce429250429496981ab4405e3
joao.santana <joao.santana@grupokrs.com.br>
2021-04-17 23:59:08 -0300

    REFAC: Adicionando a animacao de Loading no index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
+import LoadingSpinner from '~atoms/LoadingSpinner';
 import { ThemeContext } from '~contexts/themeContext';
-import SoonTemplate from '~templates/SoonTemplate';
+import HomeTemplate from '~templates/sections/Home';
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
+  const { ready } = useTranslation('SoonTemplate');
+
   return (
     <body className={theme}>
-      <SoonTemplate />
+      <SwitchTransition>
+        <CSSTransition key={ready ? 0 : 1} timeout={100} classNames="fade">
+          {ready ? <HomeTemplate /> : <LoadingSpinner loading={ready} />}
+        </CSSTransition>
+      </SwitchTransition>
     </body>
   );
 };
