ce59b071a2a3b2b47b0178634dd0a0259aaed7a4
joao.santana <joao.santana@grupokrs.com.br>
2021-05-24 20:01:29 -0300

    FEAT: Estrutura do componente molecular 'ProjectCards' FINALIZADA!

--- src/components/molecules/ProjectCards/index.tsx
+++ src/components/molecules/ProjectCards/index.tsx
@@ -3,28 +3,27 @@ import { useTranslation } from 'react-i18next';
 // eslint-disable-next-line css-modules/no-unused-class
 import styles from './styles.module.scss';
 
-// type Props = {
-//   projectTitle: string;
-//   projectDescription: string;
-//   projectImage: string;
-// };
+type Props = {
+  projectTitle: string;
+  projectDescription: string;
+  projectImage: string;
+};
 
-const ProjectsCards = () => {
-  const { t } = useTranslation('ProjectSkills');
+const ProjectsCards = ({ projectTitle, projectDescription, projectImage }: Props) => {
+  const { t } = useTranslation('ProjectsSection');
 
   return (
-    <div className={styles.projectCard}>
+    <div className={styles.projectCard} role="project-card">
       <div className={styles.projectCard__projectInformations}>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</h3>
+        <h1>{projectTitle}</h1>
+        <h3>{projectDescription}</h3>
         <div className={styles.projectCard__projectInformations__buttonsContainer}>
           <a href="">{t('projectButton')}</a>
+          <hr />
           <a href="">{t('roadmapButton')}</a>
         </div>
       </div>
-      <div className={styles.projectCard__projectImage}>
-        <img src="" alt="" />
-      </div>
+      <div className={styles.projectCard__projectImage} style={{ backgroundImage: `url(${projectImage})` }}></div>
     </div>
   );
 };
